3db6067c5352a3ef8e36aac2d5a3133a99cfc5e0
Paul Gordon <gordonp@ucalgary.ca>
2024-08-13 16:59:44 -0600

    Python wrapper for shell script, to parameterize ref genome location from config.yaml

---
+++ workflow/scripts/download_testdata.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# To be executed from Tamor root dir
+with open("config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+subprocess.run("workflow/scripts/download_testdata.sh", config["ref_fasta"])
