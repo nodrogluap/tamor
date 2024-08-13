0a876a74ce3aab99a26582b46258224727a0a1f9
Paul Gordon <gordonp@ucalgary.ca>
2024-08-13 16:50:13 -0600

    Fixed comments, added shebang, remove SRA dirs

--- workflow/scripts/download_testdata.py
+++ workflow/scripts/download_testdata.py
@@ -1,4 +1,6 @@
+#!env bash
 # Go to where Tamor expects the FASTQs in its default config.
+mkdir -p results/analysis/primary/HiSeq
 cd results/analysis/primary/HiSeq
 
 # Use the SRA tools (install in the test conda env) to fetch a publicly available sequencing dataset.
@@ -12,14 +14,17 @@ prefetch SRR6702601
 fastq-dump -F --gzip --split-3 SRR6702602
 fastq-dump -F --gzip --split-3 SRR6702601
 
+# Remove the SRA archive directories now that we have the FASTQ files extracted
+rm -rf SRR6702602 SRR6702601
+
 # There is no "normal" sample here, only tumor. 
-# So generate a normal-ish input, by removing most reads with somatic mutations from the FASTQ files, using a k-mer table of 30 million common nucleotide 
+# So generate a pseudo-normal input, by removing most reads with somatic mutations from the FASTQ files, using a k-mer table of 30 million common nucleotide 
 # variants in the human population, and the human reference genome k-mers (k=15):
 
 # Get the common human population variant kmers (30mers) from the FastGT tool.
 wget http://bioinfo.ut.ee/FastGT/downloads/kmer_db_WG30238282.db
 
-# Calculate the reference genome k-mers with the Jellyfish program (installed in the test conda env). Needs ~40Gb memory.
+# Calculate the reference genome k-mers with the Jellyfish program (installed in the test conda env). Needs ~40GB memory.
 jellyfish count -m 15 -s 100M -t 16 -C -o human.15mers.jf /work/chgi_common/reference_data/fasta/hg19.fa
 # Export the internal Jellyfish file formatted kmer table to text, capturing just the kmers but not their counts.
 jellyfish dump -c human.15mers.jf | cut -f 1 > human.15mers.txt
@@ -31,7 +36,7 @@ perl -F\\t -ane 'BEGIN{%h=split /(\s)/, `cat human.15mers.txt`}for my $kmer (@F[
 gzip -cd SRR6702602_2.fastq.gz | perl -ne 'BEGIN{open(H,"cat human.15mers.txt snp15|"); while(<H>){chomp; $kmer{$_}=1; tr/ACGT/TGCA/; $_ = reverse($_); $kmer{$_} = 1};$/="\n\@"}$uniq = 0; ($seq) = /^[^\n]+\n([^\n]+)/; while($seq=~/.{15}/g){if(not exists $kmer{$&}){$uniq = 1; last}}if(not $uniq){s/^\@// if $. == 1; chomp; print "\@$_\n"}' | gzip > SRR6702602_2.nonsomatic.fastq.gz
 gzip -cd SRR6702602_1.fastq.gz | perl -ne 'BEGIN{open(H,"cat human.15mers.txt snp15|"); while(<H>){chomp; $kmer{$_}=1; tr/ACGT/TGCA/; $_ = reverse($_); $kmer{$_} = 1};$/="\n\@"}$uniq = 0; ($seq) = /^[^\n]+\n([^\n]+)/; while($seq=~/.{15}/g){if(not exists $kmer{$&}){$uniq = 1; last}}if(not $uniq){s/^\@// if $. == 1; chomp; print "\@$_\n"}' | gzip > SRR6702602_1.nonsomatic.fastq.gz 
 
-# Only include read pairs where both reads appear to be not have any somatic mutations.
+# Only include read pairs where both reads appear to not have any somatic mutations.
 gzip -cd SRR6702602_1.nonsomatic.fastq.gz | perl -ne 'print "$1\n" if /^\@(\d+)/' > r1
 gzip -cd SRR6702602_2.nonsomatic.fastq.gz | perl -ne 'print "$1\n" if /^\@(\d+)/' > r2
 sort r1 r2 | uniq -d > keeper_read_ids
