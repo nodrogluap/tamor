a1823c6e9efd4f6c86d394bdb53645a852aeb53b
Paul Gordon <gordonp@ucalgary.ca>
2024-08-16 15:37:36 -0600

    Working local download of ref resources per default config.yaml

--- workflow/scripts/download_resources.py
+++ workflow/scripts/download_resources.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 import subprocess
 import os
+from urllib.parse import urlparse
 
-# To be executed from Tamor root dir
-with open("config/config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-# Check which resources files specified in the config don't already exist on the filesystem where expected.
+# Check which resources files don't already exist on the filesystem where expected by default.
 
 os.chdir("resources")
 resource_dict = {
-        "internal_pcgr": "http://insilico.hpc.uio.no/pcgr/pcgr_ref_data.grch38.20240621.tgz", 
-        "internal_vep": "https://ftp.ensembl.org/pub/release-112/variation/indexed_vep_cache/homo_sapiens_vep_112_GRCh38.tar.gz",
-        "ref_fasta": "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa", 
-        "ref_exon_annotations": "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.chr_patch_hapl_scaff.annotation.gtf.gz", 
-        "ref_genome": "https://s3.amazonaws.com/webdata.illumina.com/downloads/software/dragen/resource-files/hg38-alt_masked.cnv.graph.hla.rna-9-r3.0-1.tar",
-        "ref_ora": "https://s3.amazonaws.com/webdata.illumina.com/downloads/software/dragen/resource-files/misc/oradata_homo_sapiens.tar"
+        "internal_pcgr": ["http://insilico.hpc.uio.no/pcgr/pcgr_ref_data.grch38.20240621.tgz", "data", ["tar","xzf"]],
+        "internal_vep": ["https://ftp.ensembl.org/pub/release-112/variation/indexed_vep_cache/homo_sapiens_vep_112_GRCh38.tar.gz","homo_sapiens", ["tar","xzf"]],
+        "ref_fasta": ["http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa", "GRCh38_full_analysis_set_plus_decoy_hla.fa"],
+        "ref_exon_annotations": ["https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.chr_patch_hapl_scaff.annotation.gtf.gz", "gencode.v46.chr_patch_hapl_scaff.annotation.gtf.gz"],
+        "ref_genome": ["https://s3.amazonaws.com/webdata.illumina.com/downloads/software/dragen/resource-files/hg38-alt_masked.cnv.graph.hla.rna-9-r3.0-1.tar", "kmer_cnv.bin", ["tar"]],
+        "ref_ora": ["https://s3.amazonaws.com/webdata.illumina.com/downloads/software/dragen/resource-files/misc/oradata_homo_sapiens.tar", "oradata_homo_sapiens", ["tar"]]
 }
-for resource_name in resource_names.keys():
-    if(not os.path.isfile(config[resource_name])):
+for resource_name, spec in resource_dict.items():
+    if(not os.path.exists(spec[1])):
+        print("Fetching missing resource " + resource_name +  " from " + spec[0])
         # Adding -c so we can resume interrupted large downloads if possible
-        os.subprocess(["wget", "-c", config[resource_name]])
-        
+        subprocess.run(["wget", "-c", spec[0]])
+        # Post-process as required
+        if length(spec) == 3:
+            a = urlparse(spec[0])
+            spec[3].append(os.path.basename(a.path))
+            subprocess.run(spec[3])
