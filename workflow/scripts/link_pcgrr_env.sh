7ddf0096e2f9011143cd124455b12fe0e2f2ee64
Paul Gordon <gordonp@ucalgary.ca>
2024-10-14 15:02:17 -0600

    Changed to relative path

--- workflow/scripts/link_pcgrr_env.sh
+++ workflow/scripts/link_pcgrr_env.sh
@@ -2,4 +2,4 @@
 # Snakemake automatically creates the dir on rule invocation, but we want a softlink, so remove the empty dir that Snakemake created first.
 rmdir .snakemake/conda/pcgrr/bin
 rmdir .snakemake/conda/pcgrr
-echo `which Rscript` | perl -ne 'system "ln -s `pwd`/$1 .snakemake/conda/pcgrr" if /^(.*)\/bin\/Rscript/'
+echo `which Rscript` | perl -ne 'if(/^(.*)\/bin\/Rscript/){$path=$1; ($dir,$rel_path) = $path =~ /^(.*\.snakemake\/conda)\/(.+)$/; system "cd $dir; ln -s $rel_path pcgrr"}'
