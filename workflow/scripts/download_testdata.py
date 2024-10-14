bb0a228ab52a844701ee2c3245c1c23be5e1d800
Paul Gordon <gordonp@ucalgary.ca>
2024-10-14 14:20:21 -0600

    Script default permissions now executable

--- workflow/scripts/download_testdata.py
+++ workflow/scripts/download_testdata.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import subprocess
+import yaml
 
 # To be executed from Tamor root dir
 with open("config/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-subprocess.run("workflow/scripts/download_testdata.sh", config["ref_fasta"])
+subprocess.run(["workflow/scripts/download_testdata.sh", config["ref_fasta"]])
