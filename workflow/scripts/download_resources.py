dd2b97c9e5518e3ae947a21d9a0af63b70142675
Paul Gordon <gordonp@ucalgary.ca>
2025-03-05 13:45:44 -0700

    Fixed error message, updated PCGR data bundle version

--- workflow/scripts/download_resources.py
+++ workflow/scripts/download_resources.py
@@ -13,7 +13,7 @@ except OSError:
 print("INFO: Changed working directory to 'resources'")
 
 resource_dict = {
-        "internal_pcgr": ["http://insilico.hpc.uio.no/pcgr/pcgr_ref_data.20240927.grch38.tgz", "data", "tar", "xzf"],
+        "internal_pcgr": ["http://insilico.hpc.uio.no/pcgr/pcgr_ref_data.20250221.grch38.tgz", "data/grch38", "tar", "xzf"],
         "internal_vep": ["https://ftp.ensembl.org/pub/release-113/variation/indexed_vep_cache/homo_sapiens_vep_113_GRCh38.tar.gz","homo_sapiens/113_GRCh38", "tar", "xzf"],
         "ref_exon_annotations": ["https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.chr_patch_hapl_scaff.annotation.gtf.gz", "gencode.v46.chr_patch_hapl_scaff.annotation.gtf.gz"],
         "ref_genome": ["https://s3.amazonaws.com/webdata.illumina.com/downloads/software/dragen/resource-files/hg38-alt_masked.cnv.graph.hla.rna-9-r3.0-1.tar", "kmer_cnv.bin", "tar", "xf"],
@@ -29,12 +29,12 @@ for resource_name, spec in resource_dict.items():
         # Adding -c so we can resume interrupted large downloads if possible
         result = subprocess.run(["wget", "--progress=bar:force:noscroll", "-c", spec[0], "--output-document", local_path+".part"])
         if result.returncode != 0:
-            raise SystemExit("FATAL: Aborting reference data download, received non-zero return code ("+result.returncode+") from command: " +
+            raise SystemExit("FATAL: Aborting reference data download, received non-zero return code ("+str(result.returncode)+") from command: " +
             "wget --progress=bar:force:noscroll -c " + spec[0] + "--output-document "+ local_path+".part ")
 
         result = subprocess.run(["mv", local_path+".part", local_path])
         if result.returncode != 0:
-            raise SystemExit("FATAL: Received non-zero return code ("+result.returncode+") from command: mv " + local_path+".part" + local_path)
+            raise SystemExit("FATAL: Received non-zero return code ("+str(result.returncode)+") from command: mv " + local_path+".part" + local_path)
 
         # Post-process as required, and assume we can delete the source file after this.
         if len(spec) >= 3:
@@ -44,12 +44,12 @@ for resource_name, spec in resource_dict.items():
             print("INFO: Running post-processing command:", post_process_cmd, sep=" ")
             result = subprocess.run(post_process_cmd)
             if result.returncode != 0:
-                raise SystemExit("FATAL: Aborting reference data download, received non-zero return code ("+result.returncode+") from command: " + post_process_cmd)
+                raise SystemExit("FATAL: Aborting reference data download, received non-zero return code ("+str(result.returncode)+") from command: " + post_process_cmd)
 
             print("INFO: Removing source file after post-processing: "+ local_path)
             result = subprocess.run(["rm", local_path])
             if result.returncode != 0:
-                print("WARNING: Received non-zero return code ("+result.returncode+") from source cleanup command: rm " + local_path)
+                print("WARNING: Received non-zero return code ("+str(result.returncode)+") from source cleanup command: rm " + local_path)
 
 # Illumina Annotation Engine (used for Tumor Mutational Burden reporting) has its own downloading system, in a standard place on Dragen servers
 nirvana_downloader_path = "/opt/edico/share/nirvana/Downloader"
@@ -62,4 +62,4 @@ if(not os.path.exists("nirvana")):
 print("INFO: Fetching Nirvana annotation databases with " + nirvana_downloader_path)
 result = subprocess.run([nirvana_downloader_path, "--ga", "GRCh38", "--out", "nirvana"])
 if result.returncode != 0:
-    raise SystemExit("FATAL: Received non-zero return code (" + result.returncode + ") from command: " + nirvana_downloader_path + " --ga GRCh38 --out nirvana")
+    raise SystemExit("FATAL: Received non-zero return code (" + str(result.returncode) + ") from command: " + nirvana_downloader_path + " --ga GRCh38 --out nirvana")
