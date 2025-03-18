0b9d1e718bf1b5a5bc243012ed52d2d815f1937b
Paul Gordon <gordonp@ucalgary.ca>
2025-03-17 21:31:53 -0600

    Made more flexible script in case an exiting PCGR env is updated

--- workflow/scripts/link_pcgrr_env.sh
+++ workflow/scripts/link_pcgrr_env.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-# Snakemake automatically creates the dir on rule invocation, but we want a softlink, so remove the empty dir that Snakemake created first.
-rmdir .snakemake/conda/pcgrr/bin
-rmdir .snakemake/conda/pcgrr
+
+# Create a soft link in the Snakemake conda cache dir so PCGR can incoke an env called pcgrr by name even though it's managed and anonymous nomrally in the Snakemake world.
+
+# If the pcgrr link is already existing but need to be updated (e.g. because the pcgrr pin or yaml in env changed), remove the existing link.
+if [ -L .snakemake/conda/pcgrr ]; then
+  rm -f .snakemake/conda/pcgrr
+
+# otherwise, if it's never been set up here before, Snakemake automatically creates the dir on rule invocation, but we want a softlink, so remove the empty dir that Snakemake created first.
+elif [ -d .snakemake/conda/pcgrr ]; then
+  rmdir .snakemake/conda/pcgrr/bin
+  rmdir .snakemake/conda/pcgrr
+fi
+
 echo `which Rscript` | perl -ne 'if(/^(.*)\/bin\/Rscript/){$path=$1; ($dir,$rel_path) = $path =~ /^(.*\.snakemake\/conda)\/(.+)$/; system "cd $dir; ln -s $rel_path pcgrr"}'
