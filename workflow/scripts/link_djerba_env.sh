19616b7678a4c83b3fb058df2a4dc579a93c148b
Paul Gordon <gordonp@ucalgary.ca>
2025-04-30 20:15:19 -0600

    Added robustness for re-install/update of env

--- workflow/scripts/link_djerba_env.sh
+++ workflow/scripts/link_djerba_env.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-# Snakemake automatically creates the dir on rule invocation, but we want a softlink, so remove the empty dir that Snakemake created first.
-rmdir .snakemake/conda/djerba/bin
-rmdir .snakemake/conda/djerba
+# If the link already exists from a previous invocation, remove it.
+if [ -L .snakemake/conda/djerba ]; then
+  rm .snakemake/conda/djerba
+elif [ -d .snakemake/conda/djerba ]; then
+  # Snakemake automatically creates the dir on rule invocation, but we want a softlink, so remove the empty dir that Snakemake created first.
+  rmdir .snakemake/conda/djerba/bin
+  rmdir .snakemake/conda/djerba
+fi
 # Generate the djerba.smk rule output expected path for djerba.py via softlink
 echo `which djerba.py` | perl -ne 'if(/^(.*)\/bin\/djerba.py/){$path=$1; ($dir,$rel_path) = $path =~ /^(.*\.snakemake\/conda)\/(.+)$/; system "cd $dir; ln -s $rel_path djerba"}'
